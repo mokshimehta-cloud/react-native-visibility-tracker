a201d41e4fbb96b77c50a0b17e44cbdbcc2c7c1e
mokshi mehta <mokshi.mehta@devxconsultancy.com>
2026-03-05 19:18:04 +0530

    fix: import React in iOS native files

--- ios/VisibilityView.swift
+++ ios/VisibilityView.swift
@@ -1,4 +1,5 @@
 import UIKit
+import React
 
 class VisibilityView: UIView {
 
--- ios/VisibilityViewManager.swift
+++ ios/VisibilityViewManager.swift
@@ -1,4 +1,5 @@
 import Foundation
+import React
 
 @objc(VisibilityViewManager)
 class VisibilityViewManager: RCTViewManager {
